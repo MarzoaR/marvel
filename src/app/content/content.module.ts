c8460d8abeb450ceae57040c562c4098aaa2c690
Roberto Marzoa <robertomarzoa@gmail.com>
2021-05-29 15:18:50 -0400

    Se creo la estructura basica del proyecto

---
+++ src/app/content/content.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharactersListComponent } from './pages/characters-list/characters-list.component';
+import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
+import { ComicDetailsComponent } from './pages/comic-details/comic-details.component';
+import { ComicsListComponent } from './pages/comics-list/comics-list.component';
+import { CreatorsListComponent } from './pages/creators-list/creators-list.component';
+import { CreatorDetailsComponent } from './pages/creator-details/creator-details.component';
+import { SerieDetailsComponent } from './pages/serie-details/serie-details.component';
+import { SeriesListComponent } from './pages/series-list/series-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    CharactersListComponent,
+    CharacterDetailsComponent,
+    ComicDetailsComponent,
+    ComicsListComponent,
+    CreatorsListComponent,
+    CreatorDetailsComponent,
+    SerieDetailsComponent,
+    SeriesListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContentModule { }
---
+++ src/app/content/pages/comic-details/comic-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comic-details',
+  templateUrl: './comic-details.component.html',
+  styleUrls: ['./comic-details.component.css']
+})
+export class ComicDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
