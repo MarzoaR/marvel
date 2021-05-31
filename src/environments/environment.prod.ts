1a9ff0208884b6ca50afb5fb979fc45f03701fd2
Roberto Marzoa <robertomarzoa@gmail.com>
2021-05-31 19:01:39 -0400

    Se agregan las claves de la api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKeyPublic: '0234ed4d3662a3d4dfed0d064e69c72d',
+  apiKeyPrivate: 'e352a93477c700f3f90e2c0be9d8792351e46a43'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKeyPublic: '0234ed4d3662a3d4dfed0d064e69c72d',
+  apiKeyPrivate: 'e352a93477c700f3f90e2c0be9d8792351e46a43'
 };
 
 /*
